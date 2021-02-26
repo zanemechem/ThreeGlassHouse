87f414369cdc2385b26bd6a8f27740c11cc61e04
zanemechem <zanemechem@users.noreply.github.com>
2021-02-26 00:47:50 -0800

    equirec

--- js/threeScene.js
+++ js/threeScene.js
@@ -1,191 +1,137 @@
 //IMPORTS//
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js'
-
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js";
+import { DeviceOrientationControls } from "https://unpkg.com/three/examples/jsm/controls/DeviceOrientationControls.js";
 //SCENE//
 const scene = new THREE.Scene();
-const axesHelper = new THREE.AxesHelper(2); scene.add(axesHelper);
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 const loader = new GLTFLoader();
+scene.background = new THREE.Color(0xdddddd);
 
-loader.load( 'meshes/weedplant.glb', function( gltf ) {
-
-    gltf.scene.traverse( function( node ) {
-
-        if ( node.isMesh ) { node.castShadow = true; node.recieveShadow = true;
-        node.scale.set(4,4,4);}
-
-    } );
-
-    scene.add( gltf.scene );
-
-} );
-//CAMERA//
-const camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight,.01,1000);
-
-
-camera.position.set(50, 60,40);
-camera.lookAt(0,0,0);
+var cube = undefined;
+var weedModel = undefined;
 
 //LIGHTS//
+loader.load("meshes/weedplant_2.glb", (result) => {
+  weedModel = result.scene.children[1];
+  weedModel.traverse(n =>{
+    if(n.isMesh){
+      n.castShadow = true;
+      n.recieveShadow = true;
+      if(n.material.map) n.material.map.anisotrophy = 16;
+    }
+  })
+  scene.add(weedModel);
+  weedModel.scale.set(4,4,4);
+});
+loader.load("meshes/planeToImport.glb", (result) => {
+  const model = result.scene.children[1];
+  model.traverse(n =>{
+    if(n.isMesh){
+      n.castShadow = true;
+      n.recieveShadow = true;
+      if(n.material.map) n.material.map.anisotrophy = 16;
+    }
+  })
+  scene.add(model);
+  model.scale.set(4,4,4);
+});
 
-const c_brightWhite = 0xFFFFFF;
-const ambientIntensity = .4;
-const directIntensity = 3;
+const HemiLight = new THREE.HemisphereLight(0xffeeb1,0x080820,4);
+scene.add(HemiLight);
+const SpotLight = new THREE.SpotLight(0xffa95c,4);
+SpotLight.castShadow = true;
+SpotLight.shadow.bias = -0.0001;
+SpotLight.shadow.mapSize.width = 1024*4;
+SpotLight.shadow.mapSize.height = 1024*4;
+scene.add(SpotLight);
 
-const light = new THREE.DirectionalLight(c_brightWhite,directIntensity);
-const amli = new THREE.AmbientLight(c_brightWhite,ambientIntensity);
-light.position.set(20, 40, 20);
-light.target.position.set(0, 0, 0);
-light.castShadow = true;
-scene.add(light);
-scene.add(light.target)
-scene.add(amli);
 
-const camerahelper =  new THREE.CameraHelper(light.shadow.camera);
-scene.add(camerahelper);
 //RENDERER//
 const renderer = new THREE.WebGLRenderer();
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.shadowMap.enabled = true;
 renderer.physicallyCorrectLights = true;
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.toneMappingExposure = 1.4;
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera,renderer.domElement);
-const GroundPlane = new THREE.PlaneGeometry(100,100,2,2);
-const box = new THREE.BoxGeometry();
-const color1 = new THREE.Color("rgb(255,230,255)");
-const material = new THREE.MeshBasicMaterial({color:color1});
-const litMat = new THREE.MeshPhongMaterial({color:color1});
 
-const cube = new THREE.Mesh(box,litMat);
-cube.scale.set(5,5,5);
-const lineMat = new THREE.LineBasicMaterial({color:color1});
+//CAMERA//
+const camera = new THREE.PerspectiveCamera(
+  30,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  1000
+);
+camera.position.set(50, 60, 40);
+camera.lookAt(0, 0, 0);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-
-const plane = new THREE.Mesh(GroundPlane,litMat);
-plane.recieveShadow = true;
-plane.castShadow = true;
-plane.rotation.x = Math.PI * -.5;
-const points = [];
-points.push(new THREE.Vector3(-10,0,0));
-points.push(new THREE.Vector3(0,0,0));
-points.push(new THREE.Vector3(10,0,0));
-const LineGeo = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(LineGeo,material);
-
-scene.add(cube);
-scene.add(line);
-scene.add(plane);
-
-function rotateCube(){
+init();
 
 
-    cube.rotation.x += .01;
-    cube.rotation.y += .02;
-
-
-    
-}
-function animate(){
-    requestAnimationFrame(animate);
-    rotateCube();
-    renderer.render(scene,camera);
+function init() {
+  loadModels();
+  animate();
 }
 
-animate();
+function loadModels() {
+
+//PLANE AND BOX GEO
+  const GroundPlane = new THREE.PlaneGeometry(100, 100, 2, 2);
+  const box = new THREE.BoxGeometry();
+
+  //COLORS
+  const color1 = new THREE.Color(0xffeeb1);
+
+//MATS
+
+  const material = new THREE.MeshBasicMaterial({ color: color1 });
+  const litMat = new THREE.MeshPhongMaterial({ color: color1 });
+
+  cube = new THREE.Mesh(box, litMat);
+  cube.scale.set(5, 5, 5);
+  const lineMat = new THREE.LineBasicMaterial({ color: color1 });
+
+  //make plane
+  const plane = new THREE.Mesh(GroundPlane, material);
+  plane.recieveShadow = true;
+  plane.castShadow = true;
+  plane.rotation.x = Math.PI * -0.5;
+//GEO TO SCENE
+  //scene.add(cube);
+  //scene.add(plane);
+}
+
+
+
+
+
+function rotateCube() {
+
+  if (weedModel != undefined){weedModel.rotation.y +=0.005;}
+}
+function animate() {
+  requestAnimationFrame(animate);
+  rotateCube();
+  SpotLight.position.set(camera.position.x+10,camera.position.y +10,camera.position.z +10);
+  
+  renderer.render(scene, camera);
+}
+
+
 
 class ColorGUIHelper {
-    constructor(object, prop) {
-      this.object = object;
-      this.prop = prop;
-    }
-    get value() {
-      return `#${this.object[this.prop].getHexString()}`;
-    }
-    set value(hexString) {
-      this.object[this.prop].set(hexString);
-    }
+  constructor(object, prop) {
+    this.object = object;
+    this.prop = prop;
   }
-
-<<<<<<< HEAD
-
-			import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-
-			import { DeviceOrientationControls } from 'https://unpkg.com/three/examples/jsm/controls/DeviceOrientationControls.js';
-
-			let camera, scene, renderer, controls;
-
-
-        checkGyroscope();
-				init();
-				animate();
-
-
-
-			function init() {
-
-
-
-				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
-
-				controls = new DeviceOrientationControls( camera );
-
-				scene = new THREE.Scene();
-
-				const geometry = new THREE.SphereGeometry( 500, 60, 40 );
-				// invert the geometry on the x-axis so that all of the faces point inward
-				geometry.scale( - 1, 1, 1 );
-
-				const material = new THREE.MeshBasicMaterial( {
-					map: new THREE.TextureLoader().load( 'textures/panotest.jpg' )
-				} );
-
-				const mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );
-
-				const helperGeometry = new THREE.BoxGeometry( 100, 100, 100, 4, 4, 4 );
-				const helperMaterial = new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
-				const helper = new THREE.Mesh( helperGeometry, helperMaterial );
-				scene.add( helper );
-
-				//
-
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize );
-
-
-			}
-
-			function animate() {
-
-				window.requestAnimationFrame( animate );
-
-				controls.update();
-				renderer.render( scene, camera );
-
-			}
-
-			function onWindowResize() {
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
-
-      function checkGyroscope(){
-        var gyroPresent = false;
-        window.addEventListener("devicemotion", function(event){
-          if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)
-              gyroPresent = true;
-      });
-      }
-=======
->>>>>>> parent of 39b70b7 (a04)
+  get value() {
+    return `#${this.object[this.prop].getHexString()}`;
+  }
+  set value(hexString) {
+    this.object[this.prop].set(hexString);
+  }
+}
---
+++ js/threeScene2.js
@@ -0,0 +1,107 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
+var manualControl = false;
+			var longitude = 0;
+			var latitude = 0;
+			var savedX;
+			var savedY;
+			var savedLongitude;
+			var savedLatitude;
+			
+			// panoramas background
+			var panoramasArray = ["textures/equi.jpg","textures/equi_Rocks.jpg","textures/equi2.jpg","textures/notree.png"];
+			var panoramaNumber = Math.floor(Math.random()*panoramasArray.length);
+
+			// setting up the renderer
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			document.body.appendChild(renderer.domElement);
+			
+			// creating a new scene
+			var scene = new THREE.Scene();
+			
+			// adding a camera
+			var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+			camera.target = new THREE.Vector3(0, 0, 0);
+
+			// creation of a big sphere geometry
+			var sphere = new THREE.SphereGeometry(100, 100, 40);
+			sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+
+			// creation of the sphere material
+			var sphereMaterial = new THREE.MeshBasicMaterial();
+			sphereMaterial.map = THREE.ImageUtils.loadTexture(panoramasArray[panoramaNumber])
+
+			// geometry + material = mesh (actual object)
+			var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+			scene.add(sphereMesh);
+
+			// listeners
+			document.addEventListener("mousedown", onDocumentMouseDown, false);
+			document.addEventListener("mousemove", onDocumentMouseMove, false);
+			document.addEventListener("mouseup", onDocumentMouseUp, false);
+				
+               render();
+               
+               function render(){
+				
+				requestAnimationFrame(render);
+				
+				if(!manualControl){
+					longitude += 0.01;
+				}
+
+				// limiting latitude from -85 to 85 (cannot point to the sky or under your feet)
+                    latitude = Math.max(-85, Math.min(85, latitude));
+
+				// moving the camera according to current latitude (vertical movement) and longitude (horizontal movement)
+				camera.target.x = 500 * Math.sin(THREE.Math.degToRad(90 - latitude)) * Math.cos(THREE.Math.degToRad(longitude));
+				camera.target.y = 500 * Math.cos(THREE.Math.degToRad(90 - latitude));
+				camera.target.z = 500 * Math.sin(THREE.Math.degToRad(90 - latitude)) * Math.sin(THREE.Math.degToRad(longitude));
+				camera.lookAt(camera.target);
+
+				// calling again render function
+				renderer.render(scene, camera);
+				
+			}
+			
+			// when the mouse is pressed, we switch to manual control and save current coordinates
+			function onDocumentMouseDown(event){
+
+				event.preventDefault();
+
+				manualControl = true;
+
+				savedX = event.clientX;
+				savedY = event.clientY;
+
+				savedLongitude = longitude;
+				savedLatitude = latitude;
+
+			}
+
+			// when the mouse moves, if in manual contro we adjust coordinates
+			function onDocumentMouseMove(event){
+
+				if(manualControl){
+					longitude = (savedX - event.clientX) * 0.1 + savedLongitude;
+					latitude = (event.clientY - savedY) * 0.1 + savedLatitude;
+				}
+
+			}
+
+			// when the mouse is released, we turn manual control off
+			function onDocumentMouseUp(event){
+
+				manualControl = false;
+
+			}
+			
+			// pressing a key (actually releasing it) changes the texture map
+			document.onkeyup = function(event){
+			
+				panoramaNumber = (panoramaNumber + 1) % panoramasArray.length
+				sphereMaterial.map = THREE.ImageUtils.loadTexture(panoramasArray[panoramaNumber])
+			
+    			}
+			
